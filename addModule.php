3b74ec040c616ae8b8b437f2baf1058d539ed378
Severin <severin.weigold@edu.tbz.ch>
2018-12-13 09:50:03 +0100

    modul eintragen fertig

--- addModule.php
+++ addModule.php
@@ -8,15 +8,23 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-        <script src="functions.js"></script>
         <?php include 'functions.php'; ?>
 
         <title>NoserYoung Noten-Tool</title>
-
-        <meta charset="utf-8">
     </head>
 
     <body>
+
+        <script>function backToBerufsbildner() {window.location.href = "berufsbildner.php";}</script>
+
+        <div class="container">
+            <p id="message">Loading...</p>
+        </div>
+
         <?php addModule(); ?>
+
+        <div class="container">
+            <button class="btn" onclick="backToBerufsbildner()">Zurück zum Eingabeformular</button>
+        </div>
     </body>
 </html>
