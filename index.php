4df56aa63462aa976cd16d6db244a36d13c8ab21
Larry-B4 <larissa.bosshard@edu.tbz.ch>
2018-12-13 09:26:01 +0100

    buttons eingefügt

--- index.php
+++ index.php
@@ -1,21 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" text="html/css" href="style.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="functions.js"></script>
     <?php include 'functions.php'; ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" text="html/css" href="styles.css">
 
     <title>NoserYoung Noten-Tool</title>
 
     <meta charset="utf-8">
 </head>
 <body>
+
+<!-- navbar start -->
+<img src="logo.png">
+    <ul>
+    <li><a class="active" href="#home">Home</a></li>
+    <li><a href="#contact">Module und Noten</a></li>
+    <li><a href="#about">Eintragen von Noten</a></li>
+    </ul>
+
+<!-- navbar -->
+
+
+<button class="button button1">Green</button>
+<button class="button button2">Blue</button>
+
 
 </body>
 </html>
--- lernender.php
+++ lernender.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" text="html/css" href="style.css">
+    <link rel="stylesheet" text="html/css" href="styles.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
